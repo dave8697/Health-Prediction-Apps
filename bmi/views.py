520eded3b4eb357a7a80d039692ca3dbb27bb058
Sanket <gupta.sanket007@gmail.com>
2021-03-29 22:16:10 +0530

    Updated Views

--- bmi/views.py
+++ bmi/views.py
@@ -4,3 +4,19 @@ from django.shortcuts import render
 
 def bmi(request):
     return render(request, 'bmi.html', {})
+
+def mass(request):
+    context = {}
+    if request.method == "POST":
+        fname = request.POST.get('Fname')
+        lname = request.POST.get('Lname')
+        weight = request.POST.get('Weight')
+        feet = request.POST.get('Feet')
+        inches = request.POST.get('Inches')
+        weight=float(weight)
+        feet = int(feet)
+        inches = int(inches)
+        height = (feet*0.3048)+(inches*0.0254)
+        bmi = round(weight/(height*height), 2)
+        context['bmi'] = bmi
+    return render(request, 'bmi_result.html', context)
