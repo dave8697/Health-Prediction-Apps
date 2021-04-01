615bbd3071cea96c3547892069d14fe0e8d7b7ef
Sanket <gupta.sanket007@gmail.com>
2021-04-01 15:13:46 +0530

    Updated Urls

--- liverApp/urls.py
+++ liverApp/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = 'liverApp'
 urlpatterns = [
-    path('health/', views.liverApp, name = 'liverApp')
+    path('health/', views.liverApp, name = 'liverApp'),
+    path('health/liver_result', views.liverApp_result, name = 'liver_result'),
 ]
