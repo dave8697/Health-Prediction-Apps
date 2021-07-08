f34addb80af9f6133e7c89471f79ecd26a899749
dave8697 <gupta.sanket007@gmail.com>
2021-07-08 12:49:58 +0530

    Minor Improvements

--- static/web/scripts/app.js
+++ static/web/scripts/app.js
@@ -2,6 +2,15 @@ function onlyNumber(evt) {
          
     // Only ASCII character in that range allowed
     var ASCIICode = (evt.which) ? evt.which : evt.keyCode
+    if (ASCIICode > 31 && (ASCIICode < 48 && ASCIICode != 46 || ASCIICode > 57))
+        return false;
+    return true;
+}
+
+function onlyAge(evt) {
+         
+    // Only ASCII character in that range allowed
+    var ASCIICode = (evt.which) ? evt.which : evt.keyCode
     if (ASCIICode > 31 && (ASCIICode < 48 || ASCIICode > 57))
         return false;
     return true;
@@ -13,7 +22,7 @@ document.forms[0].onsubmit=function()
     var letters = /^[A-Za-z\s]+$/;
     if(!name.match(letters))
     {
-        alert("\nInvalid Name: Please enter a valid First name");
+        alert("\nInvalid Name: Please enter a valid name");
         return false;
     }
     else
