37b3555c9696cfc6873b0287e6eb241d0917984d
dave8697 <gupta.sanket007@gmail.com>
2021-07-08 12:08:50 +0530

    Minor Improvements

---
+++ static/web/scripts/bmi_check.js
@@ -0,0 +1,28 @@
+document.forms[0].onsubmit=function()
+{
+    var fname = this.elements[1].value;
+    var letters = /^[A-Za-z\s]+$/;
+    if(!fname.match(letters))
+    {
+        alert("\nInvalid Name: Please enter a valid First name"+fname);
+        return false;
+    }
+
+    var lname = this.elements[2].value;
+    var letters = /^[A-Za-z\s]+$/;
+    if(!lname.match(letters))
+    {
+        alert("\nInvalid Name: Please enter a valid Last name");
+        return false;
+    }
+
+    var email = this.elements[5].value;
+    var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    if (emailPattern.test(email) == false)
+    {
+        alert("\nEmail should be in the format: abc@xyz.com");
+        return false;
+    }
+    else
+    return true;
+}
